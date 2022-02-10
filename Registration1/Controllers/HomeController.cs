13b7f7e0115b6da53e9da7209210dc8f0abb554e
Rajesh <annuraj261998@gmail.com>
2022-02-10 11:20:31 +0530

    Dropdwon List added

--- Registration1/Controllers/HomeController.cs
+++ Registration1/Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace Registration.Controllers
 
         public ActionResult Index()
         {
+            var list = new List<string>() { "Pune", "Mumbai", "Nagpur", "Dhule", "Nashik", "Jalgaon" };
+            var list1 = new List<string>() { "Khed", "Kurla", "Kamptee", "Sakri", "Peth", "Bhusawal" };
+            var list2 = new List<string>() { "Kheshivapur", "Jainpur", "Ajani", "Chorwad", "Abheti", "Fekari" };
+            ViewBag.list = list;
+            ViewBag.list1 = list1;
+            ViewBag.list2 = list2;
             return View();
         }
 
